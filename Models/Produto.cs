d0601876f39a50c16fc429dd710bcd43ac7873aa
LuanDevv <luangcestari2006@gmail.com>
2024-06-20 10:38:19 -0300

    alteração AppDbContext, Models, Repositories

--- Models/Produto.cs
+++ Models/Produto.cs
@@ -8,15 +8,30 @@ namespace CardapioWeb.Models
     {
         [Key]
         public int Id { get; set; }
-        [Required(ErrorMessage = "Campo obrigatório")]
+
         [Display(Name = "Nome do Produto")]
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [StringLength(100, ErrorMessage = "Limite de até {0} caracteres")]
         public string Nome { get; set; }
+
+        [Display(Name = "Descrição curta do produto")]
         [Required(ErrorMessage = "Campo obrigatório")]
         public string Descricao_Curta { get; set; }
+
+        [Display(Name = "Descrição detalhada do produto")]
+        [Required(ErrorMessage = "Campo obrigatório")]
         public string Descricao_Detalhada { get; set; }
+
+        [Display(Name = "Preço do Produto")]
+        [Required(ErrorMessage = "Campo obrigatório")]
         public decimal Preco { get; set; }
+
+        [Display(Name = "Produto preferido")]
         public bool Produto_Preferido { get; set; }
+
+        [Display(Name = "Produto em estoque")]
         public bool Em_Estoque { get; set; }
+
         public virtual Categoria Categoria { get; set; }
     }
 }
